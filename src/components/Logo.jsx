bd49e80f1d2c434924f92eed0458beb04a27e421
somya singh <somya.21b0131037@abes.ac.in>
2026-08-22 13:26:53 +0530

    logo size fixed

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,10 @@ export default function Logo({ light = false }) {
   return (
     <Link to="/" className="flex items-center gap-3 shrink-0 group">
       <img
-        src="/force-fitness-logo.png"
+        // src="/force-fitness-logo.png"
+        src="/logo4.png"
         alt="Force Fitness Gym & Spa logo"
-        className="h-14 w-auto sm:h-16 lg:h-20 object-contain"
+        className="h-18 w-[100px] xs:h-20 xs:w-51 sm:h-23 sm:w-48 lg:h-30 lg:w-[130px] md:h-20 md:w-[120px]  object-center object-cover"
       />
     </Link>
   );
